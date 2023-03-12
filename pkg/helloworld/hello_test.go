fde24c39f80bda80c44ef56a06607fb6b8c57195
Tyler Yep <tyler.yep@robinhood.com>
2023-03-12 01:15:14 -0800

    Add pre-commit

--- pkg/helloworld/hello_test.go
+++ pkg/helloworld/hello_test.go
@@ -1,15 +1,16 @@
 package helloworld
 
 import (
-    "testing"
-    "github.com/stretchr/testify/assert"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 // TestHelloName calls greetings.Hello with a name, checking
 // for a valid return value.
 func TestHelloName(t *testing.T) {
-    name := "Tyler"
-    msg, err := SayHello(name)
-    assert.Nil(t, err)
-    assert.Equal(t, msg, "Hello Tyler!", "Says hello to Tyler.")
+	name := "Tyler"
+	msg, err := SayHello(name)
+	assert.Nil(t, err)
+	assert.Equal(t, msg, "Hello Tyler!", "Says hello to Tyler.")
 }
